c4c458d5bc75bf0c73a3ebcb2d4275ae27ae0253
GiulioNencini <giulionencini2003@gmail.com>
2025-01-15 12:10:35 +0100

    Test di modifica

---
+++ cmake-build-debug/CMakeFiles/Sistema_di_gioco_Not_The_End.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sistema_di_gioco_Not_The_End.dir/main.cpp.obj"
+  "CMakeFiles/Sistema_di_gioco_Not_The_End.dir/main.cpp.obj.d"
+  "Sistema_di_gioco_Not_The_End.exe"
+  "Sistema_di_gioco_Not_The_End.exe.manifest"
+  "Sistema_di_gioco_Not_The_End.pdb"
+  "libSistema_di_gioco_Not_The_End.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sistema_di_gioco_Not_The_End.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
